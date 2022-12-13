8b169c7e95c6e97180625117c4225a5d29e5dbb8
Николай Федосеев <xneek@mail.ru>
2022-12-13 21:28:35 +0300

    pohbot

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/bot.js',
     output: {
-        filename: 'index.js',
+        filename: 'bot.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    target: 'node12.18'
+    target: 'node12.18',
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'package.json') },
+            ],
+        }),
+    ]
 };
